0eb9b491ac05bd237eadc2a95365579cb57e9b01
zhouyajun <zyj665582>
2019-09-12 10:45:25 +0800

    modify article_add view function

--- web/views/article/views.py
+++ web/views/article/views.py
@@ -1,3 +1,8 @@
+from flask import render_template, current_app, request
+
+from web.model.model import Category
+from web.utils.decorators import admin_required
+from web.exception import UnknownException
 from . import article
 
 
@@ -6,6 +11,17 @@ def articles():
     pass
 
 
-@article.route('/add',methods=['POST'])
+@article.route('/add', methods=['POST', 'GET'])
+@admin_required
 def add_article():
-    pass
+    if request.method == "POST":
+        pass
+    try:
+        categories = Category.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return UnknownException()
+    info = {
+        'categories': categories
+    }
+    return render_template('admin/article_add.html', info=info)
