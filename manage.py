666b6df1a229c1214e86f1ca98926a2cda7cccee
zhouyajun <zyj665582>
2019-11-10 21:42:26 +0800

    修改APP

--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from web import create_app, db
 
+app = create_app("pro")
+
 
 def main():
-    app = create_app("pro")
     manager = Manager(app)
     Migrate(app, db)  # 关联APP和db
     manager.add_command('db', MigrateCommand)
